ae2a94589994c890327efc80d5b354b0b9f44915
김광진 <rhkdrod12@gmail.com>
2022-10-06 17:46:47 +0900

    수정수정

--- src/main/java/com/example/practicejpa/controller/LoginController.java
+++ src/main/java/com/example/practicejpa/controller/LoginController.java
@@ -1,28 +1,34 @@
 package com.example.practicejpa.controller;
 
-import com.example.practicejpa.auth.MemberDto;
 import com.example.practicejpa.auth.UserLoginService;
 import com.example.practicejpa.exception.GlobalException;
 import com.example.practicejpa.utils.Jwt.JWTResult;
+import com.example.practicejpa.utils.Jwt.JwtProvider;
+import com.example.practicejpa.utils.Jwt.JwtState;
+import com.example.practicejpa.utils.codeMessage.SystemCode;
 import com.example.practicejpa.utils.codeMessage.SystemMessage;
 import com.example.practicejpa.utils.other.ParamUtils;
 import com.example.practicejpa.utils.responseEntity.CommResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/auth")
 public class LoginController {
 	
 	@Autowired
 	UserLoginService userLoginService;
+	@Autowired
+	JwtProvider jwtProvider;
 	
 	@PostMapping("/refreshToken")
-	
 	public ResponseEntity<?> refreshToken(@RequestParam(name = "accessToken") String accessToken, @RequestParam(name = "refreshToken") String refreshToken) {
 		
 		if (ParamUtils.isEmpty(accessToken) || ParamUtils.isEmpty(refreshToken)) {
@@ -32,5 +38,13 @@ public class LoginController {
 		return CommResponse.done(new JWTResult(userLoginService.rePublishToken(accessToken, refreshToken)));
 	}
 	
+	@PostMapping("/access")
+	public ResponseEntity<?> accessValid(HttpServletRequest request) {
+		String accessToken = request.getHeader(SystemCode.AUTHORIZATION_HEADER);
+		if (ParamUtils.isNotEmpty(accessToken)) {
+			return CommResponse.done(jwtProvider.validAccessToken(accessToken) == JwtState.SUCCESS);
+		}
+		return CommResponse.done(false);
+	}
 	
 }
